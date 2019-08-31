f2e86ae6fad467d25b095f6fcfb23cd34702e597
Marshall Polaris <marshall@pol.rs>
2019-08-30 17:27:50 -0700

    Move CF stuff out

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 const { Habitat } = require("./habitat");
 const { ParameterStore } = require("./parameter-store");
-const { CloudFormation } = require("./cloud-formation");
 
-module.exports = {
-  Habitat,
-  ParameterStore,
-  CloudFormation
-};
+module.exports = { Habitat, ParameterStore };
